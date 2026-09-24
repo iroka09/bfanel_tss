715c345d75bc8760ab1362c47914d234411a8bee
Iroka Ntomchukwu <irokatochukwu@gmail.com>
2026-09-24 02:41:50 +0100

    x

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,36 +2,7 @@
 import { useState } from "react"
 import { Link } from "@tanstack/react-router";
 
-/*
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-
-function GoogleMap() {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
-  return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyBBX5fS1Vgtm3tEBs8i75nfr7HjFQRuqqA" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          // lat={59.955413}
-          //  lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
-  );
-}
-*/
 
 
 export default function App() {
